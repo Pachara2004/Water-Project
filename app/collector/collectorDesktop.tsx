8fef46c7d52b3bbefcfd09be8fae7f82f76a1657
Pachara2004 <pachara000004@gmail.com>
2026-07-23 09:22:36 +0700

    refactor: ปรับแยก ui desktop กับ mobile

---
+++ app/collector/collectorDesktop.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { CollectorProps } from "./collectorMobile";
+
+export default function CollectorDesktop({ samples, loading }: CollectorProps) {
+    return (
+        <div className="min-h-dvh w-full bg-bg p-8">
+            <h1 className="text-xl font-bold text-text">Collector Dashboard (Desktop View)</h1>
+            {/* UI Desktop วางตรงนี้ */}
+        </div>
+    );
+}
